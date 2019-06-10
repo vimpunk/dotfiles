3f7e62511d78de34661372587f824e6dc1ec8c02
mandreyel <mandreyel@protonmail.com>
2019-06-10 18:49:05 +0200

    Add sqlite and diesle_cli and fix rls and rustfmt installs

--- set-up-ubuntu.sh
+++ set-up-ubuntu.sh
@@ -19,11 +19,18 @@ sudo apt-get install \
     software-properties-common
 
 # env
-sudo apt -y install vim i3 dmenu compton feh scrot stow git zsh curl ssh silversearcher-ag
+sudo apt -y install vim vim-gnome i3 dmenu compton feh scrot stow git zsh curl ssh silversearcher-ag
 
+# vim anywhere
+curl --fail --silent --show-error --location \
+    https://raw.github.com/cknadler/vim-anywhere/master/install | bash
+
+# dev tools
+sudo apt -y install shellcheck gcc g++ clang nodejs npm postgresql libpq-dev redis cmake golang sqlite3 sqlite3-dev
 # docker
 # add docker gpg keys
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+curl --fail --silent --show-error --location \
+    https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 # docker stable repo ppa (note: use cosmic release as there are currently no
 # stable docker releases for disco)
 sudo add-apt-repository \
@@ -35,9 +42,6 @@ sudo apt -y install docker-ce
 # add user to docker group so we can connect to the docker daemon socket
 sudo usermod -a -G docker "${USER}"
 
-# dev tools
-sudo apt -y install shellcheck gcc g++ clang nodejs npm postgresql libpq-dev redis cmake golang
-
 # postgres doesn't link correctly right off the bat, because the linker can't
 # find libpq.so as there will only be a libpq.so.n, so create a symlink to that
 sudo ln -s /lib/x86_64-linux-gnu/libpq.so.[0-9] /lib/x86_64-linux-gnu/libpq.so
@@ -46,7 +50,9 @@ sudo ln -s /lib/x86_64-linux-gnu/libpq.so.[0-9] /lib/x86_64-linux-gnu/libpq.so
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 # source cargo so it's available in this shell
 source ~/.cargo/env
-cargo install rls rustfmt sccache
+cargo install sccache
+cargo install diesel_cli --no-default-features --features "postgres sqlite"
+rustup component add rls rust-analysis rust-src rustfmt
 
 ############################################################
 # configuration
