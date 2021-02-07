dffb3de29f2166dc7d970af304f0b3698f91ead4
mandreyel <mandreyel@protonmail.com>
2021-02-07 16:13:49 +0100

    Refactor setup script for legibility

--- set-up-ubuntu.sh
+++ set-up-ubuntu.sh
@@ -3,203 +3,332 @@
 set -o nounset    # error when referencing undefined variable
 set -o errexit    # exit when command fails
 
+ubuntu_release="$(lsb_release --codename --short)"
+echo "Setting up Ubuntu ${ubuntu_release}.\n\n"
+
+# Prints the error message given as the first argument and exits.
+function error {
+    echo "[ERROR]: $1."
+    echo "<<<<<< ABORTING SETUP >>>>>>"
+    exit 1
+}
+
+function start_section {
+    echo "<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<"
+    echo $1
+}
+
+function end_section {
+    echo $1
+    echo ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
+}
+
+# Applies the configuration of the first argument, which is a unit in the
+# dotfiles repository.
+function apply_conf {
+    echo "Configuring $1."
+    (
+        cd $HOME/dotfiles
+        if [ ! -e $1 ]; then
+            error "$1 is not a saved configuration"
+        fi
+        # TODO: add check for whether target of stow exists
+        stow $1
+    )
+}
+
+############################################################
+# PRE-INSTALL
+############################################################
+
 # execute everything from $HOME
 cd "${HOME}"
 
-ubuntu_release="$(lsb_release --codename --short)"
-echo "Setting up Ubuntu ${ubuntu_release}"
+# tools required to run further installation steps
+# NOTE: has to come first
+function before_install {
+    start_section "Running pre-install hook."
 
-############################################################
-# installs
-############################################################
+    sudo apt -y update
 
-sudo apt -y update
+    # packages to allow apt to use a repo over HTTPS
+    sudo apt -y install \
+        apt-transport-https \
+        ca-certificates \
+        curl \
+        ssh \
+        stow \
+        git \
+        software-properties-common
 
-# packages to allow apt to use a repo over HTTPS
-sudo apt -y install \
-    apt-transport-https \
-    ca-certificates \
-    curl \
-    software-properties-common
+    echo "Cloning dotfiles repo."
+    git clone https://github.com/mandreyel/dotfiles
 
-# general env
-sudo apt -y install \
-    vim \
-    vim-gtk3 \
-    dmenu \
-    feh \
-    scrot \
-    stow \
-    git \
-    zsh \
-    curl \
-    ssh \
-    silversearcher-ag \
-    xclip \
-    mpv \
-    sshfs \
-    htop \
-    iftop
+    end_section "Pre-install hook complete."
+}
 
-# vim anywhere
-curl --fail --silent --show-error --location \
-    https://raw.github.com/cknadler/vim-anywhere/master/install | bash
-
-# dev tools
-sudo apt -y install \
-    shellcheck \
-    gcc \
-    g++ \
-    clang \
-    nodejs \
-    npm \
-    postgresql \
-    libpq-dev \
-    redis \
-    cmake \
-    golang \
-    sqlite3 \
-    libsqlite3-dev \
-    libmysqlclient-dev
-
-# docker
-#
-# add docker gpg keys
-curl --fail --silent --show-error --location \
-    https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-# docker stable repo ppa
-sudo add-apt-repository \
-   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
-   ${ubuntu_release} \
-   stable"
-sudo apt -y update
-sudo apt -y install docker-ce
-# add user to docker group so we can connect to the docker daemon socket
-sudo usermod -a -G docker "${USER}"
-
-if [ ! -d ~/.local/bin ]; then
-    mkdir -p ~/.local/bin 
-fi
-
-# node version manager and latest node
-wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.37.2/install.sh | bash
-# export nvm for this session to install node
-export NVM_DIR=$HOME/.nvm
-$NVM_DIR/nvm.sh
-# install latest node
-nvm install node
-
-# neovim
-(
-    cd ~/.local/bin
-    curl --location \
-        --output nvim \
-        https://github.com/neovim/neovim/releases/download/stable/nvim.appimage
-    chmod u+x nvim
-)
+before_install
 
 ################################################################################
-# regolith
+# DEV
 ################################################################################
 
-sudo add-apt-repository ppa:regolith-linux/release
+# compilers, linters, etc
+function install_lang_toolchains {
+    start_section "Installing various language toolchains."
 
-if [ "${device}" == "laptop" ]; then
-    sudo apt install regolith-desktop-mobile
-else
-    sudo apt install regolith-desktop-standard
-fi
+    # scripts
+    sudo apt -y install \
+        shellcheck
 
-# set colorscheme
-regolith-look set solarized-dark
-# memory status indicator
-sudo apt install i3xrocks-memory
+    # C/C++ toolchain
+    if [ "${skip_cpp}" != true ]; then
+        echo "Installing C/C++ toolchains."
+        sudo apt -y install \
+            cmake \
+            gcc \
+            g++ \
+            clang
+        echo "Installed gcc: $(gcc --version)"
+        echo "Installed clang: $(clang --version)"
+    fi
 
-# the regolith config will be "stowed" below
+    # dbs
+    if [ "${skip_dbs}" != true ]; then
+        echo "Installing postgres, sqlite, redis."
+        sudo apt -y install \
+            postgresql \
+            libpq-dev \
+            redis \
+            sqlite3 \
+            libsqlite3-dev
+        echo "Installed postgres: $(psql --version)"
+        echo "Installed sqlite3: $(sqlite3 --version)"
+        echo "Installed redis: $(redis-server --version)"
+    fi
 
-echo "Regolith installed. Changes will take effect on next restart."
+    # node version manager and latest node
+    if [ "${skip_node}" != true ]; then
+        echo "Installing nvm and node."
+        wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.37.2/install.sh | bash
+        # export nvm for this session to install node
+        export NVM_DIR=$HOME/.nvm
+        $NVM_DIR/nvm.sh
+        # install latest node
+        nvm install node
+        echo "Node (and nvm) installed: $(node --version)"
+    fi
+
+    end_section "Language toolchains installed."
+}
+
+install_lang_toolchains
+
+# latest docker community edition
+function install_docker {
+    start_section "Installing latest Docker community edition."
+
+    # add docker gpg keys
+    curl --fail --silent --show-error --location \
+        https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+    # docker stable repo ppa
+    sudo add-apt-repository \
+       "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+       ${ubuntu_release} \
+       stable"
+    sudo apt -y update
+    sudo apt -y install docker-ce
+    # add user to docker group so we can connect to the docker daemon socket
+    sudo usermod -a -G docker "${USER}"
+
+    end_section "Docker installed: $(docker --version)"
+}
+
+install_docker
+
+function setup_rust {
+    start_section "Installing latest Rust toolchain."
+
+    curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+
+    # this takes a _long_ time and can be skipped if not needed
+    if [ "${skip_rust_tools}" != true ]; then
+        # source cargo so it's available in this shell
+        source ~/.cargo/env
+
+        # dev tools
+        rustup component add \
+            rls \
+            rust-analysis \
+            rust-src \
+            clippy \
+            rustfmt
+
+        # cargo tools
+        cargo install \
+            cargo-bloat \
+            cargo-feature-analyst \
+            cargo-audit \
+            cargo-outdated \
+            cargo-expand \
+            git-delta
+
+        # db tools
+        if [ "${skip_dbs}" != true ]; then
+            cargo install \
+                diesel_cli \
+                sqlx_cli
+        fi
+    fi
+
+    end_section "Rust installed: $(rustc --version)"
+}
+
+setup_rust
 
 ################################################################################
-# rust
+# SHELL & DESKTOP ENV
 ################################################################################
 
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
-# source cargo so it's available in this shell
-source ~/.cargo/env
-cargo install \
-    cargo-bloat \
-    cargo-feature-analyst \
-    cargo-audit \
-    cargo-outdated \
-    cargo-expand \
-    diesel_cli \
-    sqlx_cli \
-    git-delta \
-    ripgrep \
-    tokei \
-    starship \
-    evxcr_repl
-rustup component add \
-    rls \
-    rust-analysis \
-    rust-src \
-    clippy \
-    rustfmt
+function setup_regolith {
+    sudo add-apt-repository ppa:regolith-linux/release
 
-# rust analyzer (need to build from source for now)
-# TODO: check if vim-coc doesn't already take care of this
-#(
-    #cd /tmp
-    #git clone https://github.com/rust-analyzer/rust-analyzer.git
-    #cd rust-analyzer
-    ## only build the lsp binary, we don't need the vscode extension
-    #cargo xtask install --server
-#)
+    if [ "${device}" == "laptop" ]; then
+        start_section "Installing latest Regolith for mobile."
+        sudo apt -y install regolith-desktop-mobile
+    else
+        end_section "Installing latest Regolith for desktop."
+        sudo apt -y install regolith-desktop-standard
+    fi
 
-############################################################
-# configuration
-############################################################
+    # colorscheme
+    regolith-look set solarized-dark
 
-# change shell to zsh
-sudo usermod -s /usr/bin/zsh "${USER}"
+    # memory status indicator
+    sudo apt install -y i3xrocks-memory
 
-# clone configuration repo
-git clone https://github.com/mandreyel/dotfiles
+    # override default config
+    apply_conf regolith
 
-# remove default config files (if they exist) as otherwise stow will fail
-[ -f ~/.zshrc ] && rm ~/.zshrc
-[ -f ~/.bashrc ] && rm ~/.bashrc
+    end_section "Regolith installed & configured. Changes will take effect on next restart."
+}
 
-(
-    cd dotfiles
+setup_regolith
 
-    git submodule update --init --recursive
+function setup_shell {
+    start_section "Installing shell and related tools."
 
-    # apply local ($HOME) config files
-    stow vim
-    stow zsh
-    stow bash
-    stow bin
-    stow git
-    stow regolith
+    sudo apt -y install \
+        zsh \
+        feh \
+        scrot \
+        silversearcher-ag \
+        xclip
 
-    # apply global config files
+    cargo install \
+        starship \
+        ripgrep \
+        tokei \
+        evxcr_repl
+
+    # change shell to zsh
+    sudo usermod -s /usr/bin/zsh "${USER}"
+
+    # remove default config files (if they exist) as otherwise stow will fail
+    [ -f ~/.zshrc ] && rm ~/.zshrc
+    [ -f ~/.bashrc ] && rm ~/.bashrc
+
+    apply_conf zsh
+    apply_conf bash
+    apply_conf git
+    apply_conf bin
+
+    # zsh relies on submodules, install these
     (
-        cd etc
-        # delete default zsh config file and use own
-        sudo rm /etc/zsh/zshrc
-        sudo stow -t /etc/zsh zsh
+        cd dotfiles
+        git submodule update --init --recursive
+
+        # apply global config files
+        (
+            cd etc
+            # delete default zsh config file and use own
+            sudo rm /etc/zsh/zshrc
+            sudo stow -t /etc/zsh zsh
+        )
+
     )
-)
 
-############################################################
-# vim
-############################################################
+    end_section "Shell installed and configured."
+}
 
-# install all plugins
-vim +PlugInstall +qall
+setup_shell
 
-# install coc-nvim plugins
-# FIXME: this doesn't work
-# possibly look at https://github.com/neoclide/coc.nvim/wiki/Install-coc.nvim#automation-script
-vim "+CocInstall coc-rls coc-lists" +qall
+# installs vim, vim-gtk3, nvim, vim-anywhere, and configures them
+function setup_vim {
+    start_section "Installing vim and related tools."
+
+    sudo apt -y install \
+        vim \
+        vim-gtk3
+
+    # vim anywhere
+    curl --fail --silent --show-error --location \
+        https://raw.github.com/cknadler/vim-anywhere/master/install | bash
+
+    # neovim
+    if [ "${skip_nvim}" != true ]; then
+        echo "Installing neovim."
+        (
+            if [ ! -d ~/.local/bin ]; then
+                mkdir -p ~/.local/bin 
+            fi
+
+            cd ~/.local/bin
+            curl --location \
+                --output nvim \
+                https://github.com/neovim/neovim/releases/download/stable/nvim.appimage
+            chmod u+x nvim
+        )
+        echo "Neovim installed: $(nvim --version)"
+    fi
+
+    # configure
+    apply_conf vim
+    # install all plugins
+    vim +PlugInstall +qall
+    # install coc-nvim plugins
+    # FIXME: this doesn't work
+    # possibly look at https://github.com/neoclide/coc.nvim/wiki/Install-coc.nvim#automation-script
+    vim "+CocInstall coc-rls coc-lists" +qall
+
+    end_section "Vim installed: $(vim --version)"
+}
+
+setup_vim
+
+# general sysadmin tools
+function install_system_tool {
+    start_section "Installing system tools."
+
+    sudo apt -y install \
+        htop \
+        iftop
+
+    end_section "System tools installed."
+}
+
+install_system_tools
+
+# non-essentials
+function install_apps {
+    start_section "Installing apps."
+
+    sudo apt -y install \
+        mpv
+
+    # TODO: signal
+
+    end_section "Apps installed."
+}
+
+install_apps
