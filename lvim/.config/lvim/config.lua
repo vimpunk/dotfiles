3950a47f628a695541d3030d3f193bd6abeba949
mandreyel <mandreyel@protonmail.com>
2022-03-26 15:26:35 +0100

    lvim: fix config

--- lvim/.config/lvim/config.lua
+++ lvim/.config/lvim/config.lua
@@ -104,6 +104,7 @@ lvim.builtin.telescope.defaults.pickers.find_files["theme"] = "dropdown"
 --------------------------------------------------------------------------------
 -- default makes quitting the editor too easy
 lvim.builtin.which_key.mappings["q"] = nil
+lvim.builtin.which_key.mappings["w"] = nil
 
 -- re-assign lvim defaults
 -- reassign git mapping from 'g' to capital 'G' to be harder to hit
@@ -120,6 +121,7 @@ lvim.builtin.which_key.mappings["<space>"] = clear_highlight
 -- new mappings
 lvim.builtin.which_key.mappings["g"] = { "<cmd>Telescope live_grep<CR>", "Grep" }
 lvim.builtin.which_key.mappings["sP"] = { "<cmd>Telescope projects<CR>", "Projects" }
+lvim.builtin.which_key.mappings["sp"] = nil
 lvim.builtin.which_key.mappings["sp"] = { "<cmd>Telescope resume<CR>", "Prev" }
 
 -- lvim.builtin.which_key.mappings["t"] = {
@@ -254,18 +256,5 @@ lvim.plugins = {
     -- },
 }
 
-require('neoscroll').setup({
-   -- All these keys will be mapped to their corresponding default scrolling animation
-   mappings = {'<C-u>', '<C-d>', '<C-b>', '<C-f>',
-               '<C-y>', '<C-e>', 'zt', 'zz', 'zb'},
-   hide_cursor = true,          -- Hide cursor while scrolling
-   stop_eof = true,             -- Stop at <EOF> when scrolling downwards
-   use_local_scrolloff = false, -- Use the local scope of scrolloff instead of the global scope
-   respect_scrolloff = false,   -- Stop scrolling when the cursor reaches the scrolloff margin of the file
-   cursor_scrolls_alone = true, -- The cursor will keep on scrolling even if the window cannot scroll further
-   easing_function = nil,        -- Default easing function
-   pre_hook = nil,              -- Function to run before the scrolling animation starts
-   post_hook = nil,              -- Function to run after the scrolling animation ends
-})
 require "lsp_signature".setup()
 require "nvim-lastplace".setup()
