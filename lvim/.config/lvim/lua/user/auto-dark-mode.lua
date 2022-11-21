b11bbccb21fed9fcea06278535a2a1f3ea856643
mandreyel <mandreyel@protonmail.com>
2022-11-21 14:55:06 +0100

    alacritty: sync bg with macos system theme (through lvim)

--- lvim/.config/lvim/lua/user/auto-dark-mode.lua
+++ lvim/.config/lvim/lua/user/auto-dark-mode.lua
@@ -1,7 +1,16 @@
 pcall(function()
   local auto_dark_mode = require('auto-dark-mode')
   auto_dark_mode.setup({
-    update_interval = 10 * 1000,
+    update_interval = 5 * 1000,
+    set_dark_mode = function()
+      vim.api.nvim_set_option('background', 'dark')
+      vim.fn.execute([[!sed -i "" -e 's/^colors: \*.*/colors: \*tokyonight_night/' ~/.config/alacritty/alacritty.yml]])
+    end,
+    set_light_mode = function()
+      vim.api.nvim_set_option('background', 'light')
+      vim.fn.executable "echo hello world > ~/hello"
+      vim.fn.execute([[!sed -i "" -e 's/^colors: \*.*/colors: \*tokyonight_day/' ~/.config/alacritty/alacritty.yml]])
+    end,
   })
   auto_dark_mode.init()
 end)
