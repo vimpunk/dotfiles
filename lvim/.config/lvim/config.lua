6f3e758e3ab62284f3265c095c2f82efef0f5bc7
mandreyel <mandreyel@protonmail.com>
2023-02-22 10:33:03 +0100

    lvim: disable project plugin to disable auto re-rooting

--- lvim/.config/lvim/config.lua
+++ lvim/.config/lvim/config.lua
@@ -10,3 +10,4 @@ require "user.whichkey"
 -- For inspiration:
 -- https://github.com/ChristianChiarulli/lvim
 -- https://github.com/abzcoding/lvim/blob/main/config.lua
+-- https://github.com/accidentaldevelopment/dotfiles/tree/main/dot_config/nvim
